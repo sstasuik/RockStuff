5457ad6349017e1805861fc0f8151904ed25d9fe
sstasuik <68166185+sstasuik@users.noreply.github.com>
2020-07-25 11:18:12 -0400

    Rock V4
    There is now a boost for the rocket.

---
+++ ver4_RocksStuff-Lesson_July-25_20/RocketFuel.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * RocketFuel is in the world to fuel the Rocket as it travels through space while avoiding Asteroids.
+ * 
+ * Shaun Stasuik
+ * Date:July 25 2020
+ * Version 4
+ */
+public class RocketFuel extends Actor
+{
+    /**
+     * Act - do whatever the GasCan wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
